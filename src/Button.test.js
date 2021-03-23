488af8a81d5378898e25bd4f53e0d73256faedeb
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-23 12:25:06 +0530

    aldskj
    alskjda

---
+++ src/Button.test.js
@@ -0,0 +1,12 @@
+
+
+//you cna use either spec.js or test.js to define the files of testing code
+// jest accepts both files/formats
+// Example: jest accepts both Button.test.js and Button.spec.json
+
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import Button from './Button';
+
+
